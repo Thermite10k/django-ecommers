beceb6ede1328c6c1356cf5ac5aa7d6ca7c02738
Ali <batashi.ali@gmail.com>
2022-01-17 17:43:58 +0330

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+//imp
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+//imd
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
