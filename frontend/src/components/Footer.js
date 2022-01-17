e4916bc3aefa8c23b23a914ee71ed4374f776149
Ali <batashi.ali@gmail.com>
2022-01-17 17:44:20 +0330

    first

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//rfce
+//imd
+import { Container, Row, Col } from 'react-bootstrap'
+function footer() {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar, Container, Nav, Row } from 'react-bootstrap'
+
+function header() {
+    return (
+        <div>
+            <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                        <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+                        a
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+                </Navbar>
+            </header>    
+        </div>
+    )
+}
+
+export default header
