f9024be34256bec641d79dcbc1eeac5685939f84
Ali <batashi.ali@gmail.com>
2022-01-19 02:06:58 +0330

    section 1 is completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,28 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 //imd
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            
+          </Routes>
+          <Routes>
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+          
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
