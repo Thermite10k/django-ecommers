135bd3719cbc8b1a1f46d0febddf15f6f48de981
Ali <batashi.ali@gmail.com>
2022-01-24 00:09:35 +0330

    Redux working

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducrs } from './reducers/productREducers';
+;
 
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducrs,
+})
 
 const initialState = {}
 
